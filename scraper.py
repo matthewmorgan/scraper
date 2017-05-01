ee9dadd44f9e1e249eae20e47b2bcef7bc8bbc5a
Matthew Morgan <mail.matt.morgan@gmail.com>
2017-05-01 15:46:52 -0400

    Rename results and keys

--- scraper.py
+++ scraper.py
@@ -8,26 +8,27 @@ def scrape(url='https://academiccommons.columbia.edu/catalog/ac:205534'):
     r = request.urlopen(url).read()
     soup = BeautifulSoup(r, "html.parser")
 
-    dts = soup.find_all('dt')
+    # start by finding all the description terms in the page, use these as our result keys
+    keys = soup.find_all('dt')
 
-    results_dict = {}
+    results = {}
 
-    for dt in dts:
+    for dt in keys:
         key = dt.text.strip()
         if (key == 'Author(s):'):
             """
             authors block has dd => span => a => span author name
             """
             spans = dt.find_next_sibling('dd').find_all('span')
-            results_dict[key] = '; '.join([span.a.span.text for span in spans if span and span.a])
+            results[key] = '; '.join([span.a.span.text for span in spans if span and span.a])
         elif (key == 'Suggested Citation:'):
             """
             citations block has dd => a
             """
             contents = dt.find_next_sibling('dd').contents
-            results_dict[key] = ''.join([content.string for content in contents if content])
+            results[key] = ''.join([content.string for content in contents if content])
         else:
             span = dt.find_next_sibling('dd').span
-            results_dict[key] = span.text if span else ''
+            results[key] = span.text if span else ''
 
-    return json.dumps(results_dict)
+    return json.dumps(results)
